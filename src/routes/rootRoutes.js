95e41e165aba0c0b2d1b67a91deed543d61bfe4a
yamapi06150 <yamapi06150@gmail.com>
2017-04-25 23:29:46 +0800

    refacts file structure

--- src/routes/rootRoutes.js
+++ src/routes/rootRoutes.js
@@ -1,68 +1,9 @@
 const check = require('check-types');
 
-
-
-const checkUndefinedAndNull = (queryLat, queryLng) => {
-    if( check.assigned(queryLat) && check.assigned(queryLng)){
-        console.log(`check.assigned(queryLat): ${check.assigned(queryLat)}`);
-        console.log(`check.assigned(queryLng): ${check.assigned(queryLng)}`);
-        console.log('lat and lng are assigned');
-        return true ;
-    }else{
-        console.log(`check.assigned(queryLat): ${check.assigned(queryLat)}`);
-        console.log(`check.assigned(queryLng): ${check.assigned(queryLng)}`);
-        console.log('lat or/and lng is/are unassigned');
-        return false ;
-    }
-}
-
-const checkLengthNotZero = (queryLat, queryLng) => {
-    if( ! check.hasLength(queryLat, 0) && ! check.hasLength(queryLng, 0) ){
-        console.log(`! check.hasLength(queryLat, 0): ${! check.hasLength(queryLat, 0)}`);
-        console.log(`! check.hasLength(queryLng, 0): ${! check.hasLength(queryLng, 0)}`);
-        console.log('lat and lng \'s size are not zero');
-        return true ;
-    }else{
-        console.log(`! check.hasLength(queryLat, 0): ${! check.hasLength(queryLat, 0)}`);
-        console.log(`! check.hasLength(queryLng, 0): ${! check.hasLength(queryLng, 0)}`);
-        console.log('lat or/and lng \'s size is/are zero');
-        return false ;
-    }
-}
-
-const checkNumber = (numberLat, numberLng) => {
-    console.log(`numberLat: ${numberLat}`);
-    console.log(`numberLng: ${numberLng}`);
-
-    if( check.number(numberLat) && check.number(numberLng) ){
-        console.log(`check.number(numberLat): ${check.number(numberLat)}`);
-        console.log(`check.number(numberLng): ${check.number(numberLng)}`);
-        console.log('lat and lng are number ');
-        return true ;
-    }else{
-        console.log(`check.number(numberLat): ${check.number(numberLat)}`);
-        console.log(`check.number(numberLng): ${check.number(numberLng)}`);
-        console.log('lat or/and lng is/are not number ');
-        return false ;
-    }
-}
-
-const checkRangeOfLatAndLng = (numberLat, numberLng) => {
-    console.log(`numberLat: ${numberLat}`);
-    console.log(`numberLng: ${numberLng}`);
-
-    if( check.inRange(numberLat, -90, 90) && check.inRange(numberLng, -180, 180) ){
-        console.log(`check.inRange(numberLat, -90, 90): ${check.inRange(numberLat, -90, 90)}`);
-        console.log(`check.inRange(numberLng, -180, 180): ${check.inRange(numberLng, -180, 180)}`);
-        console.log('lat and lng are in range ');
-        return true ;
-    }else{
-        console.log(`check.inRange(numberLat, -90, 90): ${check.inRange(numberLat, -90, 90)}`);
-        console.log(`check.inRange(numberLng, -180, 180): ${check.inRange(numberLng, -180, 180)}`);
-        console.log('lat or/and lng is/are not in range ');
-        return false ;
-    }
-}
+const checkUndefinedAndNull = require('./../utils/checkTools').checkUndefinedAndNull;
+const checkLengthNotZero = require('./../utils/checkTools').checkLengthNotZero;
+const checkNumber = require('./../utils/checkTools').checkNumber;
+const checkRangeOfLatAndLng = require('./../utils/checkTools').checkRangeOfLatAndLng;
 
 const checkLatAndLng = (queryLat, queryLng) => {
     let allCheckPassed = false ;
