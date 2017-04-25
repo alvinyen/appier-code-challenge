71bbb0c3d506d60d7362dd9b5198ed79805ff451
yamapi06150 <yamapi06150@gmail.com>
2017-04-25 20:08:05 +0800

    undefined and null check done

--- src/routes/rootRoutes.js
+++ src/routes/rootRoutes.js
@@ -1,6 +1,29 @@
+const check = require('check-types');
+
 module.exports = (app) => {
-    app.get('/test', (req, res) => {
-        console.log('http get request test ok');
-        res.send('http get request test ok');
+    app.get('/v1/ubike-station/taipei', (req, res) => {
+        const arr = [] ;
+        let allCheckPassed = false ;
+        
+        const queryLat = req.query.lat ;
+        const queryLng = req.query.lng ;
+
+        console.log(`queryLat: ${queryLat}`);
+        console.log(`queryLng: ${queryLng}`);
+
+        if( check.assigned(queryLat) && check.assigned(queryLng)){
+            allCheckPassed = true ;
+            console.log(`check.assigned(queryLat): ${check.assigned(queryLat)}`);
+            console.log(`check.assigned(queryLng): ${check.assigned(queryLng)}`);
+            console.log('lat and lng are assigned');
+        }else{
+            console.log(`check.assigned(queryLat): ${check.assigned(queryLat)}`);
+            console.log(`check.assigned(queryLng): ${check.assigned(queryLng)}`);
+            console.log('lat or/and lng is/are unassigned');
+        }
+
+        
+
+        res.send(allCheckPassed);
     });
 }
